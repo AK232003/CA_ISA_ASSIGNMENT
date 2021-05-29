c5343c18faa5adbe8b413f1287d95f8438a5477e
AK232003 <76689824+AK232003@users.noreply.github.com>
2021-05-29 16:03:37 +0530

    Add files via upload

--- implementation.c
+++ implementation.c
@@ -1,11 +1,10 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
 typedef long long ll;
 int main()
 {
     ll a,b,c;
-    cin>>a>>b;
+    scanf("%lld%lld",&a,&b);
     if(a==b) c=a+b;
     else c=a-b;
-    cout<<c<<endl;
+    printf("%lld",c);
 }
